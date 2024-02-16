236fb5a372ed79cac223e0ee17ae5357223b502f
Matteo Falco <matteo.falco1986@gmail.com>
2024-02-16 23:40:13 +0000

    Minor bug fixes set variables

--- Week15_Esame/Classes.cs
+++ Week15_Esame/Classes.cs
@@ -14,12 +14,12 @@ namespace Week15_Esame
         private string _imageUrl;
         private double _price;
 
-        public int Id { get { return _id; } set { _id = Id; } }
-        public string Name { get { return _name; } set { _name = Name; } }
-        public string Brand { get { return _brand; } set { _brand = Brand; } }
-        public string Description { get { return _description; } set { _description = Description; } }
-        public string ImageUrl { get { return _imageUrl; } set { _imageUrl = ImageUrl; } }
-        public double Price { get { return _price; } set { _price = Price; } }
+        public int Id { get { return _id; } set { _id = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Brand { get { return _brand; } set { _brand = value; } }
+        public string Description { get { return _description; } set { _description = value; } }
+        public string ImageUrl { get { return _imageUrl; } set { _imageUrl = value; } }
+        public double Price { get { return _price; } set { _price = value; } }
 
         public Product() { }
         public Product(int id, string name, string brand, string description, string imageUrl, double price)
@@ -34,8 +34,12 @@ namespace Week15_Esame
     }
     public class ShoppingCart
     {
+        private static double _totalCartAmount = 0;
+        public static double TotalCartAmount
+        {
+            get { return _totalCartAmount; } set { _totalCartAmount = value; }
+        }
         public static List<Product> CartProducts = new List<Product>();
-        public static double TotalCartAmount = 0;
         public static void SetCartAmount()
         {
             if (ShoppingCart.CartProducts.Count > 0)
